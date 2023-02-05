4be7d4355d1e77aa61806290330f26c1fe64da3f
Yonathan Assefa <yab95assefa@gmail.com>
2023-02-05 15:40:44 +0300

    initial commit
    the name changed from hospital_room_service to hospital_room_management, also an app called room_management

--- hospital_room_management/hospital_room_management/asgi.py
+++ hospital_room_management/hospital_room_management/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hospital_room_service project.
+ASGI config for hospital_room_management project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_room_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_room_management.settings')
 
 application = get_asgi_application()
--- hospital_room_management/hospital_room_management/urls.py
+++ hospital_room_management/hospital_room_management/urls.py
@@ -1,4 +1,4 @@
-"""hospital_room_service URL Configuration
+"""hospital_room_management URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
